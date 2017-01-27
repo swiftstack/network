e826a96f2f114d75902391cc539c14ea5fa62d53
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-01-27 19:04:06 +0300

    [Tests] Add synchronization

--- Tests/SocketTests/SocketTests.swift
+++ Tests/SocketTests/SocketTests.swift
@@ -2,14 +2,39 @@ import XCTest
 import Dispatch
 @testable import Socket
 
+class SingleCondition {
+    var satisfied = false
+    let condition = NSCondition()
+
+    public init() {}
+
+    func satisfy() {
+        condition.lock()
+        satisfied = true
+        condition.signal()
+        condition.unlock()
+    }
+
+    func wait() {
+        condition.lock()
+        if !satisfied {
+            condition.wait()
+        }
+        condition.unlock()
+    }
+}
+
 class SocketTests: XCTestCase {
     func testSocket() {
+        let condition = SingleCondition()
         let message = [UInt8]("Hey there!".utf8)
 
-        DispatchQueue.global(qos: .background).async {
+        DispatchQueue.global().async {
             do { // echo server
                 let socket = try Socket()
-                let client = try socket.listen(at: "127.0.0.1", port: 4444).accept()
+                try socket.listen(at: "127.0.0.1", port: 4444)
+                condition.satisfy()
+                let client = try socket.accept()
                 var buffer = [UInt8](repeating: 0, count: message.count)
                 let read = try client.read(to: &buffer)
                 _ = try client.write(bytes: buffer, count: read)
@@ -17,6 +42,8 @@ class SocketTests: XCTestCase {
                 XCTFail(String(describing: error))
             }
         }
+
+        condition.wait()
 
         do {
             let socket = try Socket()
