dd484c6d5957b52f9e138c1add93be789e5d6062
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-01-29 23:46:22 +0300

    [Tests] Add socket awaiter test

--- Tests/SocketTests/SocketTests.swift
+++ Tests/SocketTests/SocketTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Platform
 import Dispatch
 @testable import Socket
 
@@ -54,6 +55,56 @@ class SocketTests: XCTestCase {
             let read = try socket.read(to: &response)
             XCTAssertEqual(read, message.count)
             XCTAssertEqual(response, message)
+        } catch {
+            XCTFail(String(describing: error))
+        }
+    }
+
+    func testSocketAwaiter() {
+        class TestAwaiter: IOAwaiter {
+            var event: IOEvent? = nil
+            func wait(for descriptor: Descriptor, event: IOEvent) throws {
+                self.event = event
+            }
+        }
+
+        let condition = SingleCondition()
+        let message = [UInt8]("Hey there!".utf8)
+
+        DispatchQueue.global().async {
+            do { // echo server
+                let awaiter = TestAwaiter()
+                let socket = try Socket(awaiter: awaiter)
+                try socket.listen(at: "127.0.0.1", port: 4445)
+                condition.satisfy()
+                let client = try socket.accept()
+                var buffer = [UInt8](repeating: 0, count: message.count)
+
+                let read = try client.read(to: &buffer)
+                XCTAssertEqual(awaiter.event, .read)
+
+                _ = try client.write(bytes: buffer, count: read)
+                XCTAssertEqual(awaiter.event, .write)
+            } catch {
+                XCTFail(String(describing: error))
+            }
+        }
+
+        condition.wait()
+
+        do {
+            let awaiter = TestAwaiter()
+            let socket = try Socket(awaiter: awaiter)
+            _ = try socket.connect(to: "127.0.0.1", port: 4445)
+
+            _ = try socket.write(bytes: message)
+            XCTAssertEqual(awaiter.event, .write)
+
+            var response = [UInt8](repeating: 0, count: message.count)
+
+            _ = try socket.read(to: &response)
+            XCTAssertEqual(awaiter.event, .read)
+
         } catch {
             XCTFail(String(describing: error))
         }
