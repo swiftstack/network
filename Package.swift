87bd010f069a95214a68c42968f2f143473801bc
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-09-02 14:17:11 +0300

    Temporary add Stream dependency, canImport doesn't work

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
     ]
 )
 
-import class Foundation.ProcessInfo
-if ProcessInfo.processInfo.environment["Development"] == "true" {
+//import class Foundation.ProcessInfo
+//if ProcessInfo.processInfo.environment["Development"] == "true" {
     package.dependencies.append(
         .package(
             url: "https://github.com/swift-stack/stream.git",
@@ -38,4 +38,4 @@ if ProcessInfo.processInfo.environment["Development"] == "true" {
         .first(where: { $0.name == "Network" })?
         .dependencies
         .append("Stream")
-}
+//}
