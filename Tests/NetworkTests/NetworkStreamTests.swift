6404a1c466f25cc936183d63fbe09442fd81000e
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-09-20 04:11:11 +0300

    [Tests] Add NetworkStreamTests

---
+++ Tests/NetworkTests/NetworkStreamTests.swift
@@ -0,0 +1,70 @@
+import Test
+import Platform
+import Dispatch
+import AsyncDispatch
+@testable import Network
+
+class NetworkStreamTests: TestCase {
+    override func setUp() {
+        AsyncDispatch().registerGlobal()
+    }
+
+    func withSocketPair(_ body: (Socket, Socket) throws -> Void) throws {
+        let ready = DispatchSemaphore(value: 0)
+
+        let listener = try Socket()
+            .bind(to: "127.0.0.1", port: 7000)
+            .listen()
+
+        var server: Socket!
+
+        async.task {
+            do {
+                ready.signal()
+                server = try listener.accept()
+                ready.signal()
+            } catch {
+                fail(String(describing: error))
+            }
+        }
+
+        ready.wait()
+        let client = try Socket().connect(to: "127.0.0.1", port: 7000)
+        ready.wait()
+        try body(server, client)
+    }
+
+    func testNetworkStream() {
+        do {
+            try withSocketPair { server, client in
+                let serverStream = NetworkStream(socket: server)
+                let clientStream = NetworkStream(socket: client)
+
+                assertEqual(try clientStream.write([0,1,2,3,4]), 5)
+                var buffer = [UInt8](repeating: 0, count: 5)
+                assertEqual(try serverStream.read(to: &buffer), 5)
+                assertEqual(buffer, [0,1,2,3,4])
+
+                assertEqual(try serverStream.write([0,1,2,3,4]), 5)
+                buffer = [UInt8](repeating: 0, count: 5)
+                assertEqual(try clientStream.read(to: &buffer), 5)
+            }
+        } catch {
+            fail(String(describing: error))
+        }
+    }
+
+    func testNetworkStreamError() {
+        do {
+            try withSocketPair { server, client in
+                let networkStream = NetworkStream(socket: client)
+                try client.close()
+                var buffer = [UInt8]()
+                assertThrowsError(try networkStream.read(to: &buffer))
+                assertThrowsError(try networkStream.write(buffer))
+            }
+        } catch {
+            fail(String(describing: error))
+        }
+    }
+}
