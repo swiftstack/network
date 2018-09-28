1f6c30f12f8f4003515507a28191d31fba5ac374
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-09-28 03:53:03 +0300

    [Tests] Fix testSystemLogger

--- Tests/NetworkTests/SystemLoggerTests.swift
+++ Tests/NetworkTests/SystemLoggerTests.swift
@@ -2,6 +2,7 @@ import Test
 import File
 import Stream
 import Fiber
+import Platform
 @testable import Log
 @testable import Async
 @testable import Network
@@ -33,6 +34,8 @@ class SystemLoggerTests: TestCase {
     func testSystemLogger() {
         let unixPath = self.temp.appending(#function).string
         let message = "message"
+
+        unlink(unixPath)
 
         async.task {
             scope {
