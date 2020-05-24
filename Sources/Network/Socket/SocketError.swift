2f6ce7da01f19571e7d78f88e462a515b699da17
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-24 10:01:03 +0300

    [SockerError] Add few static errors

--- Sources/Network/Socket/SocketError.swift
+++ Sources/Network/Socket/SocketError.swift
@@ -1,10 +1,27 @@
 import Platform
 
-public struct SocketError: Error, CustomStringConvertible {
-    public let number = errno
-    public let description = String(cString: strerror(errno))
+public struct SocketError: Error, Equatable {
+    public let number: Int32
 
+    public init(number: Int32 = errno) {
+        self.number = number
+    }
+}
+
+extension SocketError: CustomStringConvertible {
+    public var description: String { .init(cString: strerror(errno)) }
+}
+
+extension SocketError {
     public var interrupted: Bool {
         return number == EAGAIN || number == EWOULDBLOCK || number == EINTR
+    }
+
+    public static var badDescriptor: SocketError {
+        return SocketError(number: EBADF)
+    }
+
+    public static var invalidArgument: SocketError {
+        return SocketError(number: EINVAL)
     }
 }
